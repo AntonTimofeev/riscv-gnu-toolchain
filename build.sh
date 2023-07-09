f03357ffdbcab5dff56625e95891ee68e40d3f6f
Anton <>
2023-07-09 22:39:09 +0300

    upd

--- build.sh
+++ build.sh
@@ -2,14 +2,18 @@
 
 BASE_DIR="$(pwd)"
 SCRIPT_DIR="$BASE_DIR/$(dirname "$0")"
-echo $BASE_DIR
-echo $SCRIPT_DIR
 
 cd $SCRIPT_DIR
 
-BUILD_DIR="$SCRIPT_DIR/build_dir"
-echo $BUILD_DIR
-./configure --prefix=$BUILD_DIR
+# 64-bit
+BUILD_DIR_64="$SCRIPT_DIR/build_dir_64"
+./configure --prefix=$BUILD_DIR_64
+make linux -j10
+
+# 32-bit
+BUILD_DIR_32="$SCRIPT_DIR/build_dir_32"
+make clean
+./configure --prefix=$BUILD_DIR_32 --with-arch=rv32gc --with-abi=ilp32d
 make linux -j10
 
 cd $BASE_DIR
--- path.sh
+++ path.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-BIN_DIR="$(pwd)/$(dirname "$0")/build_dir/bin"
+BIN_DIR="$(pwd)/$(dirname $BASH_SOURCE)/build_dir_64/bin:$(pwd)/$(dirname $BASH_SOURCE)/build_dir_32/bin"
 export PATH="$BIN_DIR:$PATH"
